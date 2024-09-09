a0353aaad59080e09737b85e77cd9110d5460a41
BabaScience <lebabamath@gmail.com>
2024-09-09 18:08:59 +0200

    setuping up hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import Hero from './components/Hero'
 import NavBar from './components/NavBar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <NavBar />
-      <h1>Hello World</h1>
+      <Hero />
+
     </>
   )
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './styles.css'
+
+const Hero = () => {
+  return (
+    <>
+    <div className="hero">
+        <div className="title">Online Video Tools.</div>
+        <div className="sub-title">Everything <i>Free</i>  and Secured</div>
+    </div>
+
+    </>
+  )
+}
+
+export default Hero
