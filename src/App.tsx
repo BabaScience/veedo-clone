3a9ccbc4c8b57b09bb8de445a2a9a8872ca33d61
Anonymous <no-reply@example.com>
2024-09-12 14:09:46 +0200

    hero and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
-import { useState } from 'react'
+
 import './App.css'
 import Hero from './components/Hero'
 import NavBar from './components/NavBar'
+import Tools from './components/Tools'
 
 function App() {
   
 
   return (
     <>
-      <NavBar />
-      <Hero />
+      <div className="contaiiner">
+        <div className="navbar"><NavBar/></div>
+        <div className="hero"><Hero /></div>
+        <h1 id='tools-title'>Tools</h1>
+        <div className="tooldiv">
+            <Tools />
+            <Tools />
+            <Tools />
+            <Tools />
+        </div>
+        <footer className="foot">
+          <div className="name-footer">
+            VEEDO.ONLINE
+          </div>
+          <div className="mark">
+           <p>copyright 2022</p> 
+           <p>downloader converter txt-audio compressor</p> 
+          </div>
+        </footer>
+      </div>
+     
+    
 
     </>
   )
---
+++ src/components/Tools.tsx
@@ -0,0 +1,15 @@
+
+const Tools = () => {
+  return (
+    
+  <>
+    <div className="tool">
+        <div className="img"></div>
+        <div className="desc"></div>
+    </div>
+
+  </>
+  )
+}
+
+export default Tools
